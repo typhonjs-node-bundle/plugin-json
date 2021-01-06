f5799da100c3e6d3416929a401c97e2159ef6954
typhonrt <support@typhonrt.org>
2021-01-05 20:39:58 -0800

    refactor - separate loader from init hook.

--- src/hooks/init/init.js
+++ src/hooks/init/init.js
@@ -1,35 +1,4 @@
-const json = require('@rollup/plugin-json');
-
-/**
- * Handles interfacing with the plugin manager adding event bindings to pass back a configured
- * instance of `@rollup/plugin-json`.
- */
-class PluginHandler
-{
-   /**
-    * Returns the configured input plugin for `@rollup/plugin-json`
-    *
-    * @returns {object} Rollup plugin
-    */
-   static getInputPlugin()
-   {
-      return json();
-   }
-
-   /**
-    * Wires up PluginHandler on the plugin eventbus.
-    *
-    * @param {PluginEvent} ev - The plugin event.
-    *
-    * @see https://www.npmjs.com/package/typhonjs-plugin-manager
-    *
-    * @ignore
-    */
-   static onPluginLoad(ev)
-   {
-      ev.eventbus.on('typhonjs:oclif:bundle:plugins:main:input:get', PluginHandler.getInputPlugin, PluginHandler);
-   }
-}
+const PluginLoader = require('../../loader/PluginLoader');
 
 /**
  * Oclif init hook to add PluginHandler to plugin manager.
@@ -42,7 +11,7 @@ module.exports = async function(opts)
 {
    try
    {
-      global.$$pluginManager.add({ name: '@typhonjs-node-rollup/plugin-json', instance: PluginHandler });
+      global.$$pluginManager.add({ name: '@typhonjs-node-rollup/plugin-json', instance: PluginLoader });
 
       global.$$eventbus.trigger('log:debug', `plugin-json init hook running '${opts.id}'.`);
    }
---
+++ src/loader/PluginLoader.js
@@ -0,0 +1,48 @@
+const json = require('@rollup/plugin-json');
+
+/**
+ * Handles interfacing with the plugin manager adding event bindings to pass back a configured
+ * instance of `@rollup/plugin-json`.
+ */
+class PluginLoader
+{
+   /**
+    * Returns the `package.json` module name.
+    *
+    * @returns {string[]}
+    */
+   static get pluginName() { return ['@typhonjs-node-rollup/plugin-json']; }
+
+   /**
+    * Returns the rollup plugins managed.
+    *
+    * @returns {string[]}
+    */
+   static get rollupPlugins() { return ['@rollup/plugin-json']; }
+
+   /**
+    * Returns the configured input plugin for `@rollup/plugin-json`
+    *
+    * @returns {object} Rollup plugin
+    */
+   static getInputPlugin()
+   {
+      return json();
+   }
+
+   /**
+    * Wires up PluginHandler on the plugin eventbus.
+    *
+    * @param {PluginEvent} ev - The plugin event.
+    *
+    * @see https://www.npmjs.com/package/typhonjs-plugin-manager
+    *
+    * @ignore
+    */
+   static onPluginLoad(ev)
+   {
+      ev.eventbus.on('typhonjs:oclif:bundle:plugins:main:input:get', PluginLoader.getInputPlugin, PluginLoader);
+   }
+}
+
+module.exports = PluginLoader;
